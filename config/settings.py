70ad2076cb0b28226087d7aa3167b08b2d5b258c
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-14 15:24:00 +0300

    Configure static resources

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = [str(BASE_DIR.joinpath('static'))]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
