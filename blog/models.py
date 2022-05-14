ae97cdefddeb54a3b6ca89cd79d6084ee847fbbf
Nick Mwangemi <nickmwangemi@gmail.com>
2022-05-14 16:20:51 +0300

    black and isort formatting

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.urls import reverse
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
     body = models.TextField()
 
     def __str__(self):
@@ -10,4 +15,3 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
-    
--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from .views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateView, BlogDeleteView
+from .views import (
+    BlogCreateView,
+    BlogDeleteView,
+    BlogDetailView,
+    BlogListView,
+    BlogUpdateView,
+)
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('post/new/', BlogCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
-
+    path("", BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 ]
